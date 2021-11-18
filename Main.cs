a3a8ee32d8dd3837a7c1d3987fbca51c9a167ef0
Andre Issa <andrelouisissa@gmail.com>
2021-11-18 19:26:07 +1100

    merge children possibly works now

--- Main.cs
+++ Main.cs
@@ -22,7 +22,7 @@ namespace Combloonation
             [HarmonyPostfix]
             public static void Postfix()
             {
-                Bloonspawn.MutateRounds();
+                Labloontory.MutateRounds();
             }
         }
     }
