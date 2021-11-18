2118054af8927ee0d3bb83080725bf3e44e20291
Andre Issa <andrelouisissa@gmail.com>
2021-11-18 17:22:54 +1100

    bloonfactoring

--- Main.cs
+++ Main.cs
@@ -22,7 +22,7 @@ namespace Combloonation
             [HarmonyPostfix]
             public static void Postfix()
             {
-                BloonFactory.MutateRounds();
+                Bloonspawn.MutateRounds();
             }
         }
     }
