10b2444dd2a553542c5ef428245bd94b29f1b15e
Andre Issa <andrelouisissa@gmail.com>
2021-11-26 23:13:19 +1100

    fix annular

--- RegionScalarMap.cs
+++ RegionScalarMap.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Scripts.Simulation.SMath;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using Math = Assets.Scripts.Simulation.SMath.Math;
 
@@ -7,80 +6,72 @@ namespace Combloonation
 {
     public class RegionScalarMap
     {
-
-
         public static class Maps
         {
             public static Func<float, float, float> vertical = (x, y) => y;
             public static Func<float, float, float> horizontal = (x, y) => x;
-            public static Func<float, float, float> annular = (x, y) => MagnitudeSquared(x, y);
-            public static Func<float, float, float> radial = (x, y) => PositiveArgument(x, y);
+            public static Func<float, float, float> annular = (x, y) => Magnitude(x, y);
+            public static Func<float, float, float> radial = (x, y) => PositiveArgument(x, y) % Math.TWOPI;
             public static Func<float, Func<float, float, float>> spiralArchimedian = r => (x, y) =>
             {
-                return (r * Math.Sqrt(MagnitudeSquared(x, y)) + PositiveArgument(x, y)) % Math.TWOPI;
+                return (r * Magnitude(x, y) + PositiveArgument(x, y)) % Math.TWOPI;
             };
             public static Func<float, Func<float, float, float>> spiralLogarithmic = r => (x, y) =>
             {
-                return (-r * (float)System.Math.Log(Math.Sqrt(MagnitudeSquared(x, y))) + PositiveArgument(x, y)) % Math.TWOPI;
+                return (-r * (float)System.Math.Log(Magnitude(x, y)) + PositiveArgument(x, y)) % Math.TWOPI;
             };
         }
 
         public static class Regions
         {
-            public static Func<float, float, float, float, RegionScalarMap> vertical = (xrx, xry, yrx, yry) =>
+            public static Func<float, float, float, float, RegionScalarMap> vertical = (xlo, xhi, ylo, yhi) =>
             {
-                return new RegionScalarMap(xrx, xry, yrx, yry, yrx, yry, Maps.vertical);
+                return new RegionScalarMap(xlo, xhi, ylo, yhi, ylo, yhi, Maps.vertical);
             };
 
-            public static Func<float, float, float, float, RegionScalarMap> horizontal = (xrx, xry, yrx, yry) =>
+            public static Func<float, float, float, float, RegionScalarMap> horizontal = (xlo, xhi, ylo, yhi) =>
             {
-                return new RegionScalarMap(xrx, xry, yrx, yry, xrx, xry, Maps.horizontal);
+                return new RegionScalarMap(xlo, xhi, ylo, yhi, xlo, xhi, Maps.horizontal);
             };
 
-            public static Func<float, float, float, float, RegionScalarMap> annular = (xrx, xry, yrx, yry) =>
+            public static Func<float, float, float, float, RegionScalarMap> annular = (xlo, xhi, ylo, yhi) =>
             {
-                var zrx = MagnitudeSquared(MinBound(xrx, xry), MinBound(yrx, yry));
-                var zry = MagnitudeSquared(MaxBound(xrx, xry), MaxBound(yrx, yry));
-                return new RegionScalarMap(xrx, xry, yrx, yry, zrx, zry, Maps.annular);
+                float x; float y;
+                if (xlo < 0 && 0 < xhi) x = 0; else x = MinBound(xlo, xhi);
+                if (ylo < 0 && 0 < yhi) y = 0; else y = MinBound(ylo, yhi);
+                var zlo = Magnitude(x, y);
+                var zhi = Magnitude(MaxBound(xlo, xhi), MaxBound(ylo, yhi));
+                return new RegionScalarMap(xlo, xhi, ylo, yhi, zlo, zhi, Maps.annular);
             };
 
-            public static Func<float, float, float, float, RegionScalarMap> radial = (xrx, xry, yrx, yry) =>
+            public static Func<float, float, float, float, RegionScalarMap> radial = (xlo, xhi, ylo, yhi) =>
             {
-                return new RegionScalarMap(xrx, xry, yrx, yry, 0, Math.TWOPI, Maps.radial);
+                return new RegionScalarMap(xlo, xhi, ylo, yhi, 0, Math.TWOPI, Maps.radial);
             };
 
-            public static Func<float, Func<float, float, float, float, RegionScalarMap>> spiralArchimedian = r => (xrx, xry, yrx, yry) =>
+            public static Func<float, Func<float, float, float, float, RegionScalarMap>> spiralArchimedian = r => (xlo, xhi, ylo, yhi) =>
             {
-                return new RegionScalarMap(xrx, xry, yrx, yry, 0, Math.TWOPI, Maps.spiralArchimedian(r));
+                return new RegionScalarMap(xlo, xhi, ylo, yhi, 0, Math.TWOPI, Maps.spiralArchimedian(r));
             };
 
-            public static Func<float, Func<float, float, float, float, RegionScalarMap>> spiralLogarithmic = r => (xrx, xry, yrx, yry) =>
+            public static Func<float, Func<float, float, float, float, RegionScalarMap>> spiralLogarithmic = r => (xlo, xhi, ylo, yhi) =>
             {
-                return new RegionScalarMap(xrx, xry, yrx, yry, 0, Math.TWOPI, Maps.spiralLogarithmic(r));
+                return new RegionScalarMap(xlo, xhi, ylo, yhi, 0, Math.TWOPI, Maps.spiralLogarithmic(r));
             };
         }
 
         //input information
-        public readonly float xrx;
-        public readonly float xry;
-        public readonly float yrx;
-        public readonly float yry;
-        public readonly float zrx;
-        public readonly float zry;
+        public readonly float xlo; public readonly float xhi;
+        public readonly float ylo; public readonly float yhi;
+        public readonly float zlo; public readonly float zhi;
         public readonly Func<float, float, float> f;
         //contract: the midrange of the image of f under xr cross yr is a reasonably close subset of zr
-        //          for xr, yr, zr it must be that x < y
 
-        public RegionScalarMap(float xrx, float xry, float yrx, float yry, float zrx, float zry, Func<float, float, float> f)
+        public RegionScalarMap(float xlo, float xhi, float ylo, float yhi, float zlo, float zhi, Func<float, float, float> f)
         {
-
-            if (xry < xrx) { this.xrx = xry; this.xry = xrx; }
-            else { this.xrx = xrx; this.xry = xry; }
-            if (yry < yrx) { this.yrx = yry; this.yry = yrx; }
-            else { this.yrx = yrx; this.yry = yry; }
-            if (zry < zrx) { this.zrx = zry; this.zry = zrx; }
-            else { this.zrx = zrx; this.zry = xry; }
-
+            if (xhi < xlo) { this.xlo = xhi; this.xhi = xlo; } else { this.xlo = xlo; this.xhi = xhi; }
+            if (yhi < ylo) { this.ylo = yhi; this.yhi = ylo; } else { this.ylo = ylo; this.yhi = yhi; }
+            if (zhi < zlo) { this.zlo = zhi; this.zhi = zlo; } else { this.zlo = zlo; this.zhi = xhi; }
             this.f = f;
         }
 
@@ -95,6 +86,11 @@ namespace Combloonation
             return x * x + y * y;
         }
 
+        public static float Magnitude(float x, float y)
+        {
+            return Math.Sqrt(MagnitudeSquared(x, y));
+        }
+
         public static float MaxBound(float x, float y)
         {
             return Math.Max(Math.Abs(x), Math.Abs(y));
@@ -104,28 +100,28 @@ namespace Combloonation
         {
             return Math.Min(Math.Abs(x), Math.Abs(y));
         }
-    }
 
-    public static class RSMHelperExts
-    {
         public static int SplitRange(int n, float lo, float hi, float x)
         {
             if (hi == lo) return 0;
             var d = x - lo;
             var m = hi - lo;
-            var mn = (int) m / n;
-            var i = (int) d / mn;
+            var mn = (int)m / n;
+            var i = (int)d / mn;
             return Math.Min(Math.Max(0, i), n - 1);
         }
+    }
 
+    public static class ListExt
+    {
         public static T SplitRange<T>(this List<T> list, float lo, float hi, float x)
         {
-            return list[SplitRange(list.Count, lo, hi, x)];
+            return list[RegionScalarMap.SplitRange(list.Count, lo, hi, x)];
         }
 
         public static T SplitRange<T>(this List<T> list, RegionScalarMap map, float x, float y)
         {
-            return SplitRange(list, map.zrx, map.zry, map.f(x,y));
+            return SplitRange(list, map.zlo, map.zhi, map.f(x,y));
         }
     }
 }
