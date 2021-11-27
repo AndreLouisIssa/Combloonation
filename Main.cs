ef2749fd63925f370de7c381da2a198bd14dc208
Andre Issa <andrelouisissa@gmail.com>
2021-11-28 02:45:45 +1100

    exploit idempotency to simplify displaysystem

--- Main.cs
+++ Main.cs
@@ -2,9 +2,9 @@
 using HarmonyLib;
 using Assets.Main.Scenes;
 using BTD_Mod_Helper;
-using Assets.Scripts.Unity;
 using Assets.Scripts.Utils;
 using System.IO;
+using Assets.Scripts.Unity.UI_New.InGame;
 
 [assembly: MelonInfo(typeof(Combloonation.Main), "Combloonation", "0-beta-r0", "MagicGonads")]
 [assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
@@ -30,6 +30,16 @@ namespace Combloonation
             public static void Postfix()
             {
                 Labloontory.MutateRounds();
+            }
+        }
+
+        [HarmonyPatch(typeof(InGame), nameof(InGame.Update))]
+        class Patch_InGame_Update
+        {
+            [HarmonyPostfix]
+            public static void Postfix(InGame __instance)
+            {
+                DisplaySystem.OnInGameUpdate(__instance);
             }
         }
 
