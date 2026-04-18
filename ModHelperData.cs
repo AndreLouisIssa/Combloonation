7e14691b9795daea2755850cacb3748f562dda25
Andre Louis Issa <andrelouisissa@gmail.com>
2026-04-18 16:51:08 +1000

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace Combloonation;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "54.0";
-    public const string Version = "1.0.0";
+    public const string Version = "0.2.0";
     public const string Name = "Combloonation";
 
     public const string Description = "Combining bloons in BTD6. Not guaranteed to work fully, but good enough to try out.";
