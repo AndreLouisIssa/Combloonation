b32eee5730bd614299340b0e54bd40f4a3a39c17
Andre Louis Issa <andrelouisissa@gmail.com>
2026-04-18 16:50:36 +1000

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -6,7 +6,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Combloonation";
 
-    public const string Description = "Combining bloons in BTD6";
+    public const string Description = "Combining bloons in BTD6. Not guaranteed to work fully, but good enough to try out.";
 
     public const string RepoOwner = "AndreLouisIssa";
     public const string RepoName = "Combloonation";
